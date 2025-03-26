fbab3c884212b32bc4445c6c9ea53f9a722a5167
Yajnavalkya Maddi <146632027+yajnavalkya28@users.noreply.github.com>
2025-03-26 18:08:52 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,13 +1,6 @@
-import os
-from dotenv import load_dotenv
+
 from deep_translator import GoogleTranslator
 
-def load_api_key():
-    load_dotenv()
-    api_key = os.getenv("GROQ_API_KEY")
-    if not api_key:
-        raise ValueError("No Groq API key found. Make sure you have a .env file with GROQ_API_KEY set.")
-    return api_key
 
 def translate_text(text, target_language='en'):
     """
